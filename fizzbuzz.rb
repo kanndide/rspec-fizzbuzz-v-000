e70c59bcd9e7d51941907d35ebf2c6bea3f95a0c
William Kann <william.l.kann@gmail.com>
2017-11-27 16:44:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
- if int % 3 == 0
-   puts "Fizz"
- elsif int % 5 == 0
-   puts "Buzz"
- elsif int % 3 == 0 && int % 5 == 0
-   puts "FizzBuzz"
- else
-   nil
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+  else
+    nil
  end
 end
 
