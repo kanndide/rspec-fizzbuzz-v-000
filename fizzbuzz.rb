10fb65f6e09c6cdf6fc5bdeb1666c24d31f1f18f
William Kann <william.l.kann@gmail.com>
2017-11-27 16:28:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,12 @@
 def fizzbuzz(int)
- if int % 3 == 0
-   "FizzBuzz"
- end
+  def fizzbuzz(int)
+    if int % 3 == 0
+     "Fizz"
+   elsif int % 5 == 0
+     "Buzz"
+   else
+     "FizzBuzz"
+   end
 end
 
 fizzbuzz(15)
